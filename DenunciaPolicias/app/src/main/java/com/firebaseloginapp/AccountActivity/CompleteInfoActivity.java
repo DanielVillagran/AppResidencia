addb06fe350d58e74d7c58346101834fdbca909f
Jorge Ibarra <jorge.ibarra.ms@gmail.com>
2018-11-27 17:25:31 -0600

    alv

---
+++ DenunciaPolicias/app/src/main/java/com/firebaseloginapp/AccountActivity/CompleteInfoActivity.java
@@ -0,0 +1,15 @@
+package com.firebaseloginapp.AccountActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.firebaseloginapp.R;
+
+public class CompleteInfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_info);
+    }
+}
