e8d52fca6c3d70727b83ba4f89445b3765f728f9
DanielVillagran <odvillagrana@gmail.com>
2018-11-29 21:02:33 -0600

    Commit del bebe Villagrana

--- DenunciaPolicias/app/src/main/java/com/firebaseloginapp/AccountActivity/AlumnoHome.java
+++ DenunciaPolicias/app/src/main/java/com/firebaseloginapp/AccountActivity/AlumnoHome.java
@@ -15,8 +15,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
 
